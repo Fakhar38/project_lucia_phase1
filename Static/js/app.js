9d212ff37b8dc56bc3ec1e322a7bcc26a6a66f71
Hamza Imrnist <hamza.imranist@gmail.com>
2020-03-25 00:44:41 +0500

    my static changes

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,10 +1,10 @@
 $(document).ready( function(){
     $('.filter-button').click( function() {
-        var toggleWidth = $(".filters-container").width() == "20vw" ? "0" : "20vw";
+        var toggleWidth = $(".filters-container").width() == "13.5vw" ? "0" : "13.5vw";
         $('.filters-container').animate({ width: toggleWidth });
     });
     $('.close-filters-container').click( function() {
-        var toggleWidth = $(".filters-container").width() == "0" ? "20vw" : "0";
+        var toggleWidth = $(".filters-container").width() == "0" ? "13.5vw" : "0";
         $('.filters-container').animate({ width: toggleWidth });
     });
     $('.profile-picture-div ').click( function() {
@@ -16,5 +16,13 @@ $(document).ready( function(){
         var toggleWidthPage = $(".login-page-container").width() == "0" ? "50vw" : "0";
         $('.login-page-container').animate({ width: toggleWidthPage });
         $('.login-page').css({ display: "none" });    
+    });
+    $('.fa-clone').click( function() {
+        $('.mobile-login-container').css({ display: "none" });    
+        $('.mobile-container').css({ display: "block" });    
+    });
+    $('.profile-picture-div-mob').click( function() {
+        $('.mobile-login-container').css({ display: "block" });    
+        $('.mobile-container').css({ display: "none" });    
     });
 });
