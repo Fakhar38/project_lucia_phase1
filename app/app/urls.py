0cac28e93bbe6c77a551eba05c3f21dca1fb7fea
Fakhar Mahmood <fakharmahmood38@gmail.com>
2020-03-25 00:21:45 +0500

    added ajax for web login form

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from user.views import authenticate_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authenticate/', authenticate_view, name='authenticate'),
     # re_path(r'^(?P<username>\w+)/', include('user.urls')),
     path('<str:username>/', include('user.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/user/views.py
+++ app/user/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login, authenticate
+from django.http import JsonResponse
 
 User = get_user_model()
 
 
-def store(request, username):
+def index(request, username):
     """
     Checks if username exists then renders the store page
     """
     user = get_object_or_404(User, username=username)
 
-    response = render(request, 'user/store.html', {'user': user})
+    response = render(request, 'index.html', {'user': user})
     return response
+
+
+def authenticate_view(request):
+    """
+    view to validate the login requests
+    """
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(username=username, password=password)
+    print('user', user)
+    if user:
+        login(request, user)
+        response = {
+            'authenticated': True
+        }
+        return JsonResponse(response)
+    else:
+        response = {
+            'authenticated': False
+        }
+        return JsonResponse(response, status=404)
